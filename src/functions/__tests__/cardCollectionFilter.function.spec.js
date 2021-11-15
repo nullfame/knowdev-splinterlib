6f42f2efba6f5476fdb6a7f0d888cc9baed5a4b4
Adam Finlayson <amf@adamfinlayson.com>
2021-11-15 17:11:46 -0600

    refactor: test constants

--- src/functions/__tests__/cardCollectionFilter.function.spec.js
+++ src/functions/__tests__/cardCollectionFilter.function.spec.js
@@ -7,6 +7,14 @@ const { BATTLE, CARD, FILTER, SPLINTER } = require("../../util/constants");
 // Mock constants
 //
 
+const ID = {
+  EPONA: 297,
+  GOLD_DRAGON: 59,
+  MAGGOTS: 135,
+  PEACEFUL_GIANT: 60,
+  SERPENTINE_SOLDIER: 6,
+};
+
 //
 //
 // Mock modules
@@ -44,211 +52,235 @@ describe("CardCollectionFilter function", () => {
   });
   describe("Edition", () => {
     it("Filters by edition (post-beta)", () => {
-      const sample = [cards.getTemplate(135), cards.getTemplate(297)];
+      const sample = [
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ edition: CARD.EDITION.DICE })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Filters by edition (alpha)", () => {
-      const sample = [cards.getTemplate(59), cards.getTemplate(60)];
+      const sample = [
+        cards.getTemplate(ID.GOLD_DRAGON),
+        cards.getTemplate(ID.PEACEFUL_GIANT),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ edition: CARD.EDITION.ALPHA })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Gold Dragon");
+      expect(filtered[0].id).toBe(ID.GOLD_DRAGON);
     });
     it("Filters by edition (beta)", () => {
-      const sample = [cards.getTemplate(59), cards.getTemplate(60)];
+      const sample = [
+        cards.getTemplate(ID.GOLD_DRAGON),
+        cards.getTemplate(ID.PEACEFUL_GIANT),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ edition: CARD.EDITION.BETA })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Gold Dragon");
-      expect(filtered[1].name).toBe("Peaceful Giant");
+      expect(filtered[0].id).toBe(ID.GOLD_DRAGON);
+      expect(filtered[1].id).toBe(ID.PEACEFUL_GIANT);
     });
     it("Filters by edition (alpha,beta; works like beta)", () => {
-      const sample = [cards.getTemplate(59), cards.getTemplate(60)];
+      const sample = [
+        cards.getTemplate(ID.GOLD_DRAGON),
+        cards.getTemplate(ID.PEACEFUL_GIANT),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ edition: CARD.EDITION.ALPHA_BETA })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Gold Dragon");
-      expect(filtered[1].name).toBe("Peaceful Giant");
+      expect(filtered[0].id).toBe(ID.GOLD_DRAGON);
+      expect(filtered[1].id).toBe(ID.PEACEFUL_GIANT);
     });
   });
   describe("Format", () => {
     it("Filters by format", () => {
-      const sample = [cards.getTemplate(135), cards.getTemplate(297)];
+      const sample = [
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ format: BATTLE.FORMAT.UNTAMED })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.MAGGOTS);
     });
   });
   describe("Mana", () => {
     it("Filters by exact mana (Epona)", () => {
-      const sample = [cards.getTemplate(135), cards.getTemplate(297)];
+      const sample = [
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
+      ];
       const filtered = sample.filter(cardCollectionFilter({ mana: 1 }));
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Filters by exact mana (Maggots)", () => {
-      const sample = [cards.getTemplate(135), cards.getTemplate(297)];
+      const sample = [
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
+      ];
       const filtered = sample.filter(cardCollectionFilter({ mana: 3 }));
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.MAGGOTS);
     });
     it("Filters by exact mana (Array)", () => {
-      const sample = [cards.getTemplate(135), cards.getTemplate(297)];
+      const sample = [
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
+      ];
       const filtered = sample.filter(
         cardCollectionFilter({ mana: { [FILTER.IS]: 3 } })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.MAGGOTS);
     });
     it("Filters by mana, greater than", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ mana: { [FILTER.GREATER_THAN]: 3 } })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Serpentine Soldier");
+      expect(filtered[0].id).toBe(ID.SERPENTINE_SOLDIER);
     });
     it("Filters by mana, greater than or equal", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ mana: { [FILTER.GREATER_THAN_OR_EQUAL]: 3 } })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Serpentine Soldier");
-      expect(filtered[1].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.SERPENTINE_SOLDIER);
+      expect(filtered[1].id).toBe(ID.MAGGOTS);
     });
     it("Filters by mana, less than", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ mana: { [FILTER.LESS_THAN]: 3 } })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Filters by mana, less than or equal", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ mana: { [FILTER.LESS_THAN_OR_EQUAL]: 3 } })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Maggots");
-      expect(filtered[1].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.MAGGOTS);
+      expect(filtered[1].id).toBe(ID.EPONA);
     });
   });
   describe("Rarity", () => {
     it("Rarity single", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ rarity: CARD.RARITY.LEGENDARY })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Rarity array of one", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ rarity: [CARD.RARITY.LEGENDARY] })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Rarity array of many", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ rarity: [CARD.RARITY.COMMON, CARD.RARITY.RARE] })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Serpentine Soldier");
-      expect(filtered[1].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.SERPENTINE_SOLDIER);
+      expect(filtered[1].id).toBe(ID.MAGGOTS);
     });
   });
   describe("Splinter", () => {
     it("Filters on a single splinter", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ splinter: SPLINTER.EARTH })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Filters on an array of one splinter", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ splinter: [SPLINTER.DEATH] })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.MAGGOTS);
     });
     it("Filters on an array of several splinters", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({ splinter: [SPLINTER.DEATH, SPLINTER.FIRE] })
       );
       expect(filtered.length).toBe(2);
-      expect(filtered[0].name).toBe("Serpentine Soldier");
-      expect(filtered[1].name).toBe("Maggots");
+      expect(filtered[0].id).toBe(ID.SERPENTINE_SOLDIER);
+      expect(filtered[1].id).toBe(ID.MAGGOTS);
     });
   });
   describe("Type", () => {
     it("Filters by type monster", () => {
-      const sample = [cards.getTemplate(5), cards.getTemplate(297)];
+      const sample = [cards.getTemplate(5), cards.getTemplate(ID.EPONA)];
       const filtered = sample.filter(
         cardCollectionFilter({ type: CARD.TYPE.MONSTER })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
     it("Filters by type summoner", () => {
-      const sample = [cards.getTemplate(5), cards.getTemplate(297)];
+      const sample = [cards.getTemplate(5), cards.getTemplate(ID.EPONA)];
       const filtered = sample.filter(
         cardCollectionFilter({ type: CARD.TYPE.SUMMONER })
       );
@@ -259,10 +291,10 @@ describe("CardCollectionFilter function", () => {
   describe("Multi-filter", () => {
     it("Filters on multiple attributes", () => {
       const sample = [
-        cards.getTemplate(6),
-        cards.getTemplate(135),
+        cards.getTemplate(ID.SERPENTINE_SOLDIER),
+        cards.getTemplate(ID.MAGGOTS),
         cards.getTemplate(268),
-        cards.getTemplate(297),
+        cards.getTemplate(ID.EPONA),
       ];
       const filtered = sample.filter(
         cardCollectionFilter({
@@ -271,7 +303,7 @@ describe("CardCollectionFilter function", () => {
         })
       );
       expect(filtered.length).toBe(1);
-      expect(filtered[0].name).toBe("Epona");
+      expect(filtered[0].id).toBe(ID.EPONA);
     });
   });
 });
