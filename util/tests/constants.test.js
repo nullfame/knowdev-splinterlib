829723db9e34326c29f305f92390731873d30f34
Adam Finlayson <amf@adamfinlayson.com>
2023-04-22 22:20:41 -0500

    feat: update abilities

--- util/tests/constants.test.js
+++ util/tests/constants.test.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line no-unused-vars
+const compareArrays = require("../functions/compareArrays.function");
+
 const Splinterlib = require("../../src");
 
 //
@@ -20,7 +21,20 @@ afterEach(() => {
 //
 
 describe("Data cache", () => {
-  it.todo("Abilities are up to date");
+  it("Abilities are up to date", async () => {
+    const response = await Splinterlib.cardDetailsApi();
+    const abilities = new Set();
+    response.forEach((card) => {
+      const cardAbilities = card.stats.abilities;
+      if (cardAbilities) {
+        const arr = cardAbilities.flat();
+        for (let i = 0; i < arr.length; i += 1) {
+          abilities.add(arr[i]);
+        }
+      }
+    });
+    expect(compareArrays([...abilities], Splinterlib.ALL.ABILITIES)).toBeTrue();
+  });
   it.todo("Editions are up to date");
   it.todo("Rulesets are up to date");
 });
